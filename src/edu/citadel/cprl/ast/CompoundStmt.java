c0395939ac63508310516ab8976ac813dfc42cd5
Fynn Schmidt <96319505+shark-Party@users.noreply.github.com>
2025-03-26 15:22:52 -0400

    Update CompoundStmt.java
    not quite sure what the error that needs to thrown here is, let me check the book and figure it out, just putting initial code here

--- src/edu/citadel/cprl/ast/CompoundStmt.java
+++ src/edu/citadel/cprl/ast/CompoundStmt.java
@@ -41,5 +41,10 @@ public class CompoundStmt extends Statement {
     @Override
     public void emit() throws CodeGenException {
         // ...
+        for(Statement stmt : statements) {
+    		if (stmt != null) {
+    			stmt.emit();
+    		} 
+    	}
     }
 }
